a34763a4ec798ce3bdefd79754ae1b3242858ca1
lucasgomes <lucas.roseira@fatec.sp.gov.br>
2022-08-28 11:27:03 -0300

    feat: exercicios

---
+++ VoteOrNot.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    { 
+        string ano;
+        ano = Console.ReadLine();
+        int anoInt = Convert.ToInt16(ano);
+        if(anoInt > 15) {
+            Console.WriteLine("Pode votar");
+        } else {
+            Console.WriteLine("Nao Pode votar");
+        }
+    
+    }
+
+}
